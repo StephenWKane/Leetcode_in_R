eb631bbfa1e2e441138dcbdb0d24d6d27f3f2929
StephenWKane <stephen.kane00@gmail.com>
2021-12-20 22:14:54 -0500

    Create leetcode_3.R
    A nice solution for #3.

---
+++ leetcode_3.R
@@ -0,0 +1,38 @@
+#leetcode # 3
+
+get_longest_ss_no_repeating_chars = function(x){
+
+# convert the input string into a vector of characters
+vec_s = unlist(strsplit(x,""))
+# make a vector to store unique substrings
+store_unique_strings = c(NULL)
+
+for(i in 1:length(vec_s)){
+  # initialize the running string on the first letter
+  if(i == 1){running_string = vec_s[i]} else {
+    
+    # if the current letter isn't in the running string and you aren't at the last letter then append it
+    # to the running string
+    if(!(vec_s[i] %in% running_string) & i < length(vec_s)){
+      running_string = c(running_string,vec_s[i])
+    } else {
+      # if the current letter is already in the running string then store what was in the running string
+      # in our vector of unique strings and then set the running string to the current letter
+      store_unique_strings = c(store_unique_strings,paste0(running_string,collapse = ""))
+      running_string = vec_s[i]
+    }
+  }
+}
+
+store_unique_strings_unique = unique(store_unique_strings)
+# this makes a vector of numbers corresponding to the number of characters of each string in the vector
+num_chars = nchar(store_unique_strings_unique)
+# pull the value from the unique strings that corresponds to string with maximum length.
+store_unique_strings_unique[which(num_chars == max(num_chars))]
+}
+
+s = "abcabcbb"
+get_longest_ss_no_repeating_chars(s)
+
+s = "iurtewibcdabcbbqwetry"
+get_longest_ss_no_repeating_chars(s)
