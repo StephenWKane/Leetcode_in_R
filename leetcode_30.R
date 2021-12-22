8720a138257b49cff61c3de8a49b32a01fb02c30
StephenWKane <stephen.kane00@gmail.com>
2021-12-21 19:39:00 -0500

    Create leetcode_30.R

---
+++ leetcode_30.R
@@ -0,0 +1,23 @@
+library(combinat) # a library to get permutations
+
+s = "barfoothefoobarman"
+s1 = unlist(strsplit(s,split = "")) # turn the string into a vector of characters
+
+words = c("foo","bar")
+
+# get a vector of all the permutations of the vector "words"
+words_perm = unlist(lapply(permn(words),FUN = function(x){paste0(x,collapse = "")}))
+# get the total length of each concatenation of "words"
+word_perm_length = nchar(paste0(words,collapse = ""))
+
+found = c(NULL) # keep track of which letters in s are the beginning of one of the permutations
+# stop the search before the end of the string
+for(i in 1:(length(s1) - (word_perm_length) + 1)){
+  
+  # check if the current substring is in the vector of permutations
+  if(paste0(s1[i:(i + word_perm_length - 1)],collapse = "") %in% words_perm){
+    found = c(found,i - 1)
+  }
+}
+
+found
