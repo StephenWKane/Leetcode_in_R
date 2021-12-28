f28531dd79f63be56f7a244eedbf61e0c2e47715
StephenWKane <stephen.kane00@gmail.com>
2021-12-27 22:04:14 -0500

    Update leetcode_84.R
    Modify variable name.

--- leetcode_84.R
+++ leetcode_84.R
@@ -1,18 +1,20 @@
 # leetcode 84
 
-heights = c(2,1,5,6,2,3) # the case from the problem statement
+height_vector = c(2,1,5,6,2,3) # the case from the problem statement
 
-solve = function(height){
+solve = function(heights){
 df = data.frame(start = NULL,end = NULL,area = NULL)
 for(i in 1:(length(heights) - 1)){
   starting_height = heights[i]
   for(j in (i + 1):length(heights)){
-    height = min(heights[i:j]) # the rectangle can't be taller than the smallest value in the rest of the vector
-    area = height*(j - i + 1) # the base is (j - (i - 1)) long
-    df = rbind(df,data.frame(start = i,end = j,area = area))
+    height = min(heights[i:j]) 
+    area = height*(j - i + 1) 
+    df = rbind(df,data.frame(start = i,end = j,area = area)) 
   }
 }
 df[which(df$area == max(df$area)),]$area
 }
 
-solve(heights)
+solve(height_vector)
+
+
