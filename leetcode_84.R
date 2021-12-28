d48ad15fb6d968c329e9f24f9f418a089bbd5680
StephenWKane <stephen.kane00@gmail.com>
2021-12-27 22:11:27 -0500

    Update leetcode_84.R
    Removed redundant logic.

--- leetcode_84.R
+++ leetcode_84.R
@@ -5,7 +5,6 @@ height_vector = c(2,1,5,6,2,3) # the case from the problem statement
 solve = function(heights){
 df = data.frame(start = NULL,end = NULL,area = NULL)
 for(i in 1:(length(heights) - 1)){
-  starting_height = heights[i]
   for(j in (i + 1):length(heights)){
     height = min(heights[i:j]) 
     area = height*(j - i + 1) 
@@ -16,5 +15,8 @@ df[which(df$area == max(df$area)),]$area
 }
 
 solve(height_vector)
+
+
+
 
 
