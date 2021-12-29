c0d92bf015e33db135b1aa23bf8da0f24c770487
StephenWKane <stephen.kane00@gmail.com>
2021-12-28 19:19:03 -0500

    Update leetcode_3.R

--- leetcode_3.R
+++ leetcode_3.R
@@ -4,7 +4,7 @@ solve = function(x){
 # convert the input string into a vector of characters
 vec_s = unlist(strsplit(x,""))
 # make a vector to store unique substrings
-store_unique_strings = c(NULL)
+store_strings = c(NULL)
 
 for(i in 1:length(vec_s)){
   # initialize the running string on the first letter
@@ -15,14 +15,14 @@ for(i in 1:length(vec_s)){
       running_string = c(running_string,vec_s[i])
     } else {
       # if the current letter is already in the running string then store what was in the running string
-      # in our vector of unique strings and then set the running string to the current letter
-      store_unique_strings = c(store_unique_strings,paste0(running_string,collapse = ""))
+      # in our vector of strings and then set the running string to the current letter
+      store_strings = c(store_strings,paste0(running_string,collapse = ""))
       running_string = vec_s[i]
     }
   }
 }
 
-store_unique_strings_unique = unique(store_unique_strings)
+store_unique_strings_unique = unique(store_strings)
 # this makes a vector of numbers corresponding to the number of characters of each string in the vector
 num_chars = nchar(store_unique_strings_unique)
 # pull the value from the unique strings that corresponds to the string of maximum length.
